9d4593330a5c3495f10c9bc44491f7b311d615cd
Mdafsarx <mdafsar99009@gmail.com>
2024-07-12 20:26:42 +0600

    main style half done

--- src/Components/SideNav.jsx
+++ src/Components/SideNav.jsx
@@ -1,9 +1,32 @@
+import { FaRocketchat } from "react-icons/fa";
+import { IoIosInformationCircleOutline } from "react-icons/io";
+import { IoHomeOutline, IoSettingsOutline } from "react-icons/io5";
+import {  MdOutlineNotificationsActive } from "react-icons/md";
+import { RiLogoutBoxRLine } from "react-icons/ri";
+import './text.css'
+
 export default function SideNav() {
   return (
     <div>
-        <div className="bg-dark vh-100 w-400px d-inline-block" style={{ width: '80px'}}>
+      <div className="bg-dark vh-100 d-inline-block" style={{ width: '70px' }}>
+
+        <div className="d-flex flex-column align-items-center justify-content-between vh-100">
+
+          <div className="p-3 mt-5 Text d-flex  flex-column align-items-center gap-3">
+            <p className="mt-4"><IoHomeOutline className="fs-2" /></p>
+            <p><FaRocketchat className="fs-2" /></p>
+            <p><MdOutlineNotificationsActive className="fs-2" /></p>
+            <p><IoIosInformationCircleOutline className="fs-2" /></p>
+            <p><IoSettingsOutline className="fs-2" /></p>
+          </div>
+
+          <div>
+            <p><RiLogoutBoxRLine className="fs-2 Text" /></p>
+          </div>
 
         </div>
+
+      </div>
     </div>
   )
 }
--- src/Components/TopNav.jsx
+++ src/Components/TopNav.jsx
@@ -1,22 +1,28 @@
+import { RiChatSmileLine} from "react-icons/ri"
+import img from "../assets/chatbot (2).png"
+import './text.css'
+
 
 export default function TopNav() {
     return (
         <div>
-
             <nav className="navbar navbar-expand-lg bg-dark p-3">
-                
+
                 <div className="container-fluid">
-                    <a className="navbar-brand text-white" href="#">Navbar</a>
+                    <div className="navbar-brand text-white d-flex align-items-center" > 
+                       <span className="Text2">GPT</span> <RiChatSmileLine className="Text2"/>
+                    </div>
 
                     {/* toggle-bar for small device */}
                     <button className="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
+                     {/* profile */}
                     <div className="collapse navbar-collapse">
 
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
+                            {/* <li className="nav-item">
                                 <a className="nav-link active text-white">Home</a>
                             </li>
                             <li className="nav-item">
@@ -24,20 +30,19 @@ export default function TopNav() {
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link text-white" >Disabled</a>
-                            </li>
+                            </li> */}
                         </ul>
 
                         <div className="d-flex gap-2">
                             <button className="btn btn-primary">Setting</button>
-                            <button className="btn btn-primary">Profile</button>
+                            <button className="btn btn-danger d-flex align-items-center rounded-circle"><img src={img} width={20} className="rounded-circle" alt="" /></button>
                         </div>
 
                     </div>
+
+
                 </div>
-
             </nav>
-            
-
         </div>
     )
 }
